83d4f2e3d61fd1a3cbc9307c8556c2a74614e026
Hanpu <hanpu1225@163.com>
2023-02-17 10:55:08 +0800

    2023/2/17 新增根据文章浏览数量进行排序、新增搜索文章和增加文章目录功能。

--- article/views.py
+++ article/views.py
@@ -6,18 +6,40 @@ from .forms import ArticlePostForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 
 def article_list(request):
-    articles_list = ArticlePost.objects.all()
-    #设置每页显示1篇文章
-    paginator = Paginator(articles_list,9)
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+
+    if search:
+        if order == 'total_view':
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            ).order_by('-total_view')
+        else:
+            article_list = ArticlePost.objects.filter(
+                Q(title__icontains=search) |
+                Q(body__icontains=search)
+            )
+    else:
+        search = ''
+        if order == 'total_view':
+            article_list = ArticlePost.objects.all().order_by('-total_view')
+        else:
+            article_list = ArticlePost.objects.all()
+
+    #设置每页显示6篇文章
+    paginator = Paginator(article_list,6)
     #获取url中的页码
     page = request.GET.get('page')
     #返回对应页码的文章给articles
     articles = paginator.get_page(page)
-    context = {'articles':articles}
+
+    context = {'articles':articles,'order':order}
     return render(request,'article/list.html',context)
 
 
@@ -25,15 +47,17 @@ def article_detail(request,id):
     article = ArticlePost.objects.get(id=id)
 
     #update_fields=[]指定了数据库只更新total_views字段，优化执行效率
-    article.total_view += 1
-    article.save(update_fields=['total_view'])
+    if request.user != article.author:
+        article.total_view += 1
+        article.save(update_fields=['total_view'])
 
-    article.body = markdown.markdown(article.body,
-    extensions=[
+    md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
     ])
-    context = {'article':article}
+    article.body = md.convert(article.body)
+    context = {'article':article,'toc':md.toc}
     return render(request,'article/detail.html',context)
 
 
